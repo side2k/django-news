e08870f021e4feb3c78bb75e828010cc7fa3cf5b
outsider <siderelay@gmail.com>
2011-07-27 15:32:41 +0400

    PUBLICATION_PLACES default value set to ()

--- news/settings.py
+++ news/settings.py
@@ -1,8 +1,2 @@
-from django.conf import settings as django_settings
-
-
-"""
-    Disables the latest news plugin
-    Defaults to false
-"""
-DISABLE_LATEST_NEWS_PLUGIN = getattr(django_settings, 'CMSPLUGIN_NEWS_DISABLE_LATEST_NEWS_PLUGIN', False)
+PUBLICATION_PLACES = ()
+from django.conf.settings import *
